730c0ad6cc20069669f114b93a3764f50c499f5c
Aditya Kushwaha <kushwahaaditya251@gmail.com>
2021-04-15 08:36:37 +0530

    rotate by 90 deg

--- Day1.cpp
+++ Day1.cpp
@@ -11,25 +11,49 @@ void onlinejudge() {
 #endif
 }
 
+void rotate( int a[][100] , int n ) {
+	for ( int i = 0 ; i < n  ; i++) {
+		int startCol = 0 ;
+		int endCol = n - 1 ;
+		while (startCol < endCol) {
+			swap(a[i][startCol] , a[i][endCol]) ;
+			startCol++ ;
+			endCol-- ;
+		}
+
+	}
+
+}
+void transpose ( int a[][100] , int m , int n ) {
+	for ( int i = 0 ; i < n ; i++) {
+		for ( int j = 0 ; j < n ; j++) {
+			if (i < j) {
+				swap(a[i][j] , a[j][i])  ;
+			}
+		}
+	}
+}
+
 
 int main() {
 	onlinejudge() ;
-	int m , n ;
+	int m, n ;
 	cin >> m >> n ;
-	int val = 1 ;
-	int a[m][n] ;
-	for ( int i = 0 ; i < m ; i++) {
-		for (int j = 0 ; j < n ; j++) {
-			a[i][j] = val ;
-			val++ ;
-		}
-	}
+	int a[100][100] ;
 	for (int i = 0 ; i < m ; i++) {
 		for ( int j = 0 ; j < n ; j++) {
-			cout << a[i][j] << " " ;
+			cin >> a[i][j] ;
+		}
+	}
+	rotate(a,  n) ;
+	transpose(a, m , n ) ;
+	for ( int i = 0 ; i < m  ; i++) {
+		for ( int j = 0 ; j < n ; j++) {
+			cout << a[i][j] << " "  ;
 		}
 		cout << endl ;
 	}
+
 
 
 	return 0 ;
