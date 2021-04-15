b5fc1bbfc658f5471f219fe3db9ed3cc592ed93f
Aditya Kushwaha <kushwahaaditya251@gmail.com>
2021-04-15 09:32:39 +0530

    Palindrome

--- Day1.cpp
+++ Day1.cpp
@@ -10,50 +10,34 @@ void onlinejudge() {
 	freopen("output.txt", "w", stdout);
 #endif
 }
-
-void rotate( int a[][100] , int n ) {
-	for ( int i = 0 ; i < n  ; i++) {
-		int startCol = 0 ;
-		int endCol = n - 1 ;
-		while (startCol < endCol) {
-			swap(a[i][startCol] , a[i][endCol]) ;
-			startCol++ ;
-			endCol-- ;
+bool ispalindrome(char a[100]) {
+	int i = 0 ;
+	int j = strlen(a) - 1 ;
+	while (i < j) {
+		if (a[i] == a[j]) {
+			i++ ;
+			j-- ;
+		}
+		else {
+			return false ;
 		}
 
 	}
-
-}
-void transpose ( int a[][100] , int m , int n ) {
-	for ( int i = 0 ; i < n ; i++) {
-		for ( int j = 0 ; j < n ; j++) {
-			if (i < j) {
-				swap(a[i][j] , a[j][i])  ;
-			}
-		}
-	}
+	return true ;
 }
 
 
 int main() {
 	onlinejudge() ;
-	int m, n ;
-	cin >> m >> n ;
-	int a[100][100] ;
-	for (int i = 0 ; i < m ; i++) {
-		for ( int j = 0 ; j < n ; j++) {
-			cin >> a[i][j] ;
-		}
-	}
-	rotate(a,  n) ;
-	transpose(a, m , n ) ;
-	for ( int i = 0 ; i < m  ; i++) {
-		for ( int j = 0 ; j < n ; j++) {
-			cout << a[i][j] << " "  ;
-		}
-		cout << endl ;
-	}
 
+	char str[100]  ;
+	cin >> str ;
+	if (ispalindrome(str)) {
+		cout << "It is a Palindrome" ;
+	}
+	else {
+		cout << "It is not a palindromwew " ;
+	}
 
 
 	return 0 ;
